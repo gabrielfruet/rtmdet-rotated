347fcd05b2957eb87845bd7ed4dc17042b28558b
gabrielfruet <gabrielfruet538@gmail.com>
2026-03-20 17:05:32 -0300

    feat: more tests

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -35,6 +35,75 @@ class TestRotatedTDMDetLoss:
             device=gt_boxes.device,
         )
 
+    def test__forward_single_pass__simple_close_to_zero(
+        self, example_rtmdet_loss: RotatedTDMDetLoss
+    ):
+        gt_boxes = torch.tensor([[5, 5, 10, 10, 0]], dtype=torch.float32)
+        gt_labels = torch.tensor([1])
+
+        pred_boxes = torch.tensor([[5, 5, 10, 10, 0]], dtype=torch.float32)
+
+        pred_cls_logits = torch.tensor([[-100.0, 100.0]])
+
+        strides = torch.tensor([32], dtype=torch.float32)
+
+        loss, _ = example_rtmdet_loss.forward_pass_single(
+            gt_boxes=gt_boxes,
+            gt_labels=gt_labels,
+            pred_boxes=pred_boxes,
+            pred_cls_logits=pred_cls_logits,
+            strides=strides,
+            num_classes=2,
+            device=gt_boxes.device,
+        )
+
+        assert torch.isclose(loss, torch.tensor(0.0), atol=0.001)
+
+    def test__forward_single_pass__no_ground_truth(
+        self, example_rtmdet_loss: RotatedTDMDetLoss
+    ):
+        gt_boxes = torch.empty((0, 5), dtype=torch.float32)
+        gt_labels = torch.empty(0, dtype=torch.int64)
+
+        pred_boxes = torch.tensor([[5, 5, 10, 10, 0]], dtype=torch.float32)
+
+        pred_cls_logits = torch.tensor([[-100.0, 100.0]])
+
+        strides = torch.tensor([32], dtype=torch.float32)
+
+        example_rtmdet_loss.forward_pass_single(
+            gt_boxes=gt_boxes,
+            gt_labels=gt_labels,
+            pred_boxes=pred_boxes,
+            pred_cls_logits=pred_cls_logits,
+            strides=strides,
+            num_classes=2,
+            device=gt_boxes.device,
+        )
+
+    def test__forward_single_pass__no_pred_raises_error(
+        self, example_rtmdet_loss: RotatedTDMDetLoss
+    ):
+        gt_boxes = torch.tensor([[5, 5, 10, 10, 0]], dtype=torch.float32)
+        gt_labels = torch.tensor([1])
+
+        pred_boxes = torch.empty((0, 5), dtype=torch.float32)
+
+        pred_cls_logits = torch.empty((0, 2), dtype=torch.int64)
+
+        strides = torch.empty(0, dtype=torch.float32)
+
+        with pytest.raises((RuntimeError, IndexError)):
+            example_rtmdet_loss.forward_pass_single(
+                gt_boxes=gt_boxes,
+                gt_labels=gt_labels,
+                pred_boxes=pred_boxes,
+                pred_cls_logits=pred_cls_logits,
+                strides=strides,
+                num_classes=2,
+                device=gt_boxes.device,
+            )
+
     def test__forward_single_pass__multiple(
         self, example_rtmdet_loss: RotatedTDMDetLoss
     ):
